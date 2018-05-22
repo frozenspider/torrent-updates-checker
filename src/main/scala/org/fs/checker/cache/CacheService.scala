b67da4bb3361b8c3b176bc21c66fa18d7bc5fb40
Alex Abdugafarov <fswork90@gmail.com>
2018-05-23 04:20:45 +0500

    Changed UpdateChecked to not work with config directly

--- src/main/scala/org/fs/checker/cache/CacheService.scala
+++ src/main/scala/org/fs/checker/cache/CacheService.scala
@@ -9,4 +9,8 @@ trait CacheService {
   def cache: Config
 
   def update(newCache: Config): Unit
+
+  def getCachedDetails(alias: String): Option[CachedDetails]
+
+  def updateCachedDetails(alias: String, cachedDetails: CachedDetails): Unit
 }
--- src/main/scala/org/fs/checker/cache/CacheServiceImpl.scala
+++ src/main/scala/org/fs/checker/cache/CacheServiceImpl.scala
@@ -1,19 +1,21 @@
 package org.fs.checker.cache
 
 import scala.reflect.io.File
+import scala.util.Try
 
 import org.slf4s.Logging
 
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigRenderOptions
+import com.typesafe.config.ConfigValueFactory
 
 /**
  * @author FS
  */
 class CacheServiceImpl(cacheFile: File)
-    extends CacheService
-    with Logging {
+  extends CacheService
+  with Logging {
 
   log.debug("Cache file: " + cacheFile.jfile.getAbsolutePath)
 
@@ -33,4 +35,32 @@ class CacheServiceImpl(cacheFile: File)
     } ensuring {
       cache == newCache
     }
+
+  override def getCachedDetails(alias: String): Option[CachedDetails] = {
+    val cachePrefix = "\"" + alias + "\""
+    val lastCheckMsPath = s"$cachePrefix.lastCheckMs"
+    val lastUpdateMsPath = s"$cachePrefix.lastUpdateMs"
+    if (!cache.hasPath(cachePrefix)) {
+      None
+    } else {
+      val lastCheckMsOption = Try(cache.getLong(lastCheckMsPath)).toOption
+      val lastUpdateMsOption = Try(cache.getLong(lastUpdateMsPath)).toOption
+      Some(CachedDetails(lastCheckMsOption, lastUpdateMsOption))
+    }
+  }
+
+  override def updateCachedDetails(alias: String, cachedDetails: CachedDetails): Unit = {
+    val cachePrefix = "\"" + alias + "\""
+    val lastCheckMsPath = s"$cachePrefix.lastCheckMs"
+    val lastUpdateMsPath = s"$cachePrefix.lastUpdateMs"
+    val updateMap = Map(
+      lastCheckMsPath -> cachedDetails.lastCheckMsOption,
+      lastUpdateMsPath -> cachedDetails.lastUpdateMsOption
+    )
+    val newCache = updateMap.foldLeft(cache) {
+      case (cache, (path, Some(value))) => cache.withValue(path, ConfigValueFactory.fromAnyRef(value))
+      case _                            => cache
+    }
+    update(newCache)
+  }
 }
